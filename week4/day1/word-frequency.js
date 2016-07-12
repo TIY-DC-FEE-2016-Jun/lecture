2e4ef2ab723da3101468f9658062c8f4f78c245b
Jordan Kasper <jordan@jordankasper.com>
2016-07-12 12:15:04 -0400

    Added hq solution to week 4 day 1

---
+++ week4/day1/word-frequency.js
@@ -0,0 +1,25 @@
+(function(ns) {
+    'use strict';
+
+    window.words = ns = (ns || {});
+
+    ns.count = function count(text) {
+        var wordCount = {};
+
+        text
+            .toLowerCase()
+            .replace(/\'s/g, ' ')
+            .replace(/[^a-z\-\s]/g, ' ')
+            .split(' ')
+            .filter(function excludeSingles(word) {
+                return word.length > 1;
+            })
+            .forEach(function doCount(word) {
+                wordCount[word] = (wordCount[word]) ? wordCount[word]+1 : 1;
+            });
+
+        return wordCount;
+    };
+
+
+})(window.words);
